86546fb78536848d1899617648a01c8e1ccc1f18
川上昌汰 <s2213566@coins.tsukuba.ac.jp>
2023-02-12 12:41:00 +0900

    [test]: BinExprのテストを追加

---
+++ test/testBinExpr.py
@@ -0,0 +1,24 @@
+import unittest
+from src.expression.Int import Int
+from src.expression.BinExpr import BinExpr
+
+
+class TestBinExpr(unittest.TestCase):
+    def test_type(self):
+        self.assertEqual(BinExpr("+", Int(1), Int(2)).type, "BinExpr")
+
+    def test_add(self):
+        self.assertEqual(BinExpr("+", Int(1), Int(2)).evaluate(), 3)
+
+    def test_sub(self):
+        self.assertEqual(BinExpr("-", Int(1), Int(2)).evaluate(), -1)
+
+    def test_mul(self):
+        self.assertEqual(BinExpr("*", Int(1), Int(2)).evaluate(), 2)
+
+    def test_div(self):
+        self.assertEqual(BinExpr("/", Int(1), Int(2)).evaluate(), 0)
+
+    def test_op(self):
+        with self.assertRaises(ValueError):
+            BinExpr("~", Int(1), Int(2))
