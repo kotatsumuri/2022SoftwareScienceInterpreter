3ab64efe651e84c980fcc91083d2773530b68b8f
川上昌汰 <s2213566@coins.tsukuba.ac.jp>
2023-02-13 18:00:56 +0900

    [feature]: Seqを実装

---
+++ src/expression/Seq.py
@@ -0,0 +1,16 @@
+from src.expression.Expr import Expr
+
+
+class Seq(Expr):
+    type: str
+    bodies: tuple[Expr]
+
+    def __init__(self, *bodies: Expr):
+        self.type = "Seq"
+        self.bodies = bodies
+
+    def evaluate(self, env: dict[str, int]) -> int:
+        ret = 0
+        for expr in self.bodies:
+            ret = expr.evaluate(env)
+        return ret
