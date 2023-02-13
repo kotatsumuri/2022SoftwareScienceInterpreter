d385ad29226cea3aa6ff10d4bfe3886b68e7da95
川上昌汰 <s2213566@coins.tsukuba.ac.jp>
2023-02-13 12:55:36 +0900

    [feature]: Assignmentを実装

---
+++ src/expression/Assignment.py
@@ -0,0 +1,16 @@
+from src.expression.Expr import Expr
+
+
+class Assignment(Expr):
+    type: str
+    name: str
+    expression: Expr
+
+    def __init__(self, name: str, expression: Expr):
+        self.type = "Assignment"
+        self.name = name
+        self.expression = expression
+
+    def evaluate(self, env: dict[str, int]) -> int:
+        env[self.name] = self.expression.evaluate(env)
+        return env[self.name]
