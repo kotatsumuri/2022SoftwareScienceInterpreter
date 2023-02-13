7290537ac8ea75d2a5c6c682521c20be575bed99
川上昌汰 <s2213566@coins.tsukuba.ac.jp>
2023-02-13 18:17:15 +0900

    [feature]: Whileを実装

---
+++ src/expression/While.py
@@ -0,0 +1,18 @@
+from src.expression.Expr import Expr
+
+class While(Expr):
+    type: str
+    condition: Expr
+    body: Expr
+
+    def __init__(self, condition: Expr, body: Expr):
+        self.type = "While"
+        self.condition = condition
+        self.body = body
+    
+    def evaluate(self, env: dict[str, int]) -> int:
+        ret = 0
+        while self.condition.evaluate(env):
+            ret = self.body.evaluate(env)
+        return ret
+        
