adc79452234318fcd6a1dc1be503fb86c1c0892a
川上昌汰 <s2213566@coins.tsukuba.ac.jp>
2023-02-13 20:06:25 +0900

    [fix]: 参照エラーを解消

--- src/expression/Expr.py
+++ src/expression/Expr.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
-from src.expression.Func import Func
 from typing import TypeVar
+
+Self = TypeVar("Self", bound="Expr")
 
 
 class Expr(ABC):
     @abstractmethod
-    def evaluate(self, env: dict[str, int | Func]) -> int:
+    def evaluate(self, env: dict[str, int | Self]) -> int:
         pass
